729d15cac9339e8d3688f8a20fb9f2290e6e5ea7
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-11-15 09:32:39 +0000

    Autoloader refacted

---
+++ example/Autoloader/simplest/function/conf.php
@@ -0,0 +1,4 @@
+<?php
+$conf = array(
+	"foo" => "bar"
+);
--- runtime/Autoloader/Autoloader.php
+++ runtime/Autoloader/Autoloader.php
@@ -1,75 +1,101 @@
 <?php
 class LtAutoloader
 {
-	public $classFileMapping;
+	protected $fileMapping;
+	protected $dirs;
+
+	public function __construct($dirArray = null)
+	{
+		if ($dirArray)
+		{
+			$this->dirs = $dirArray;
+      $this->scanDirs();
+			$this->init();
+		}
+	}
+
+	public function getFileMapping()
+	{
+		return $this->fileMapping;
+	}
+
+	public function setFileMapping($fileMapping)
+	{
+		$this->fileMapping = $fileMapping;
+	}
+
+	public function init()
+	{
+		$i = 0;
+		while(isset($this->fileMapping["function"][$i]))
+		{
+			include $this->fileMapping["function"][$i];
+			$i ++;
+		}
+		spl_autoload_register(array($this, "loadClass"));
+	}
 
 	public function loadClass($className)
 	{
-		if (isset($this->classFileMapping[$className]))
+		if (isset($this->fileMapping["class"][$className]))
 		{
-			include $this->classFileMapping[$className];
+			include $this->fileMapping["class"][$className];
 		}
 		else 
 		{
 			$key = strtolower($className);
-			if (isset($this->classFileMapping[$key]))
+			if (isset($this->fileMapping["class"][$key]))
 			{
-				include $this->classFileMapping[$key];
+				include $this->fileMapping["class"][$key];
 			}
 		}
 	}
 
-	public function scanDir($directories)
+	protected function addDir($dir)
 	{
-		if (is_string($directories))
+		$this->dirs[] = $dir;
+	}
+
+	protected function scanOneDir($dir)
+	{
+  	$files = scandir($dir);
+		$currentDir = realpath($dir);
+		foreach ($files as $file)
 		{
-			$directories = array($directories);
-		}
-		$mapping = array("class" => array(), "function" => array());
-		$i = 0;
-		while (isset($directories[$i]) && $files = scandir($directories[$i]))
-		{
-			$currentDir = realpath($directories[$i]);
-			foreach ($files as $file)
+			$currentFile = $currentDir . DIRECTORY_SEPARATOR . $file;
+			if (is_file($currentFile))
 			{
-				$currentFile = $currentDir . DIRECTORY_SEPARATOR . $file;
-				if (is_file($currentFile))
+				$extension = pathinfo($file, PATHINFO_EXTENSION);
+				if (in_array($extension, array("php", "inc")))// we will judge the file extension, we only accept 'php' and 'inc' file.
 				{
-					$extension = pathinfo($file, PATHINFO_EXTENSION);
-					if (in_array($extension, array("php", "inc")))// we will judge the file extension, we only accept 'php' and 'inc' file.
+					if(preg_match_all('~^\s*(?:abstract\s+|final\s+)?(?:class|interface)\s+(\w+).*~mi', trim(file_get_contents($currentFile)), $classes) > 0)
 					{
-						if(preg_match_all('~^\s*(?:abstract\s+|final\s+)?(?:class|interface)\s+(\w+).*~mi', trim(file_get_contents($currentFile)), $classes) > 0)
+						foreach($classes[1] as $key => $class)
 						{
-							foreach($classes[1] as $key => $class)
-							{
-								$mapping["class"][strtolower($class)] = $currentFile;
-							}
-						}
-						else
-						{
-							$mapping["function"][] = $currentFile;
+							$this->fileMapping["class"][strtolower($class)] = $currentFile;
 						}
 					}
-				}
-				else if(!in_array($file, array(".", "..", ".svn")))// if $currentFile is a directory, pass through the next loop.
-				{
-					$directories[] = $directories[$i] . DIRECTORY_SEPARATOR . $file;
+					else
+					{
+						$this->fileMapping["function"][] = $currentFile;
+					}
 				}
 			}
-			$i ++;
+			else if(!in_array($file, array(".", "..", ".svn")))// if $currentFile is a directory, pass through the next loop.
+			{
+				$this->dirs[] = $dir . DIRECTORY_SEPARATOR . $file;
+			}
 		}
-		return $mapping;
 	}
 
-	public function init($mapping)
+	protected function scanDirs()
 	{
+		$this->fileMapping = array("class" => array(), "function" => array());
 		$i = 0;
-		while(isset($mapping["function"][$i]))
+		while (isset($this->dirs[$i]))
 		{
-			include $mapping["function"][$i];
+			$this->scanOneDir($this->dirs[$i]);
 			$i ++;
 		}
-		$this->classFileMapping = $mapping["class"];
-		spl_autoload_register(array($this, "loadClass"));
 	}
 }
