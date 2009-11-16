228fb2cc0899f76cfb1e69ed1b6f1d95992b7c80
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-11-16 09:23:44 +0000

    加入ValidatorDtd类

--- trunk/example/Validator/simplest.php
+++ trunk/example/Validator/simplest.php
@@ -2,9 +2,10 @@
 /* 
  * 加载Validator类文件
  */
-$lotusHome = dirname(dirname(dirname(dirname(__FILE__))));
+$lotusHome = dirname(dirname(dirname(__FILE__)));
 include $lotusHome . "/runtime/Validator/Validator.php";
 include $lotusHome . "/runtime/Validator/ValidatorConfig.php";
+include $lotusHome . "/runtime/Validator/ValidatorDtd.php";
 /* 
  * 加载Validator类文件
  */
@@ -12,15 +13,13 @@ include $lotusHome . "/runtime/Validator/ValidatorConfig.php";
 /*
  * 构造验证规则
  */
-
-$dtd = array(
-	"label" => "Username",
-	"rules" => array(
+$dtd = new LtValidatorDtd("用户名",
+	array(
 		"max_length" => 4,
 		"mask" => "/^[a-z0-9]+$/i",
 		"ban" => "/fuck/",
 	),
-	"message" => array(
+	array(
 		//"max_length" 使用默认的错误消息，在$this->conf->errorMessage里
 		"mask" => "户名只能由数字或字组成",
 		"ban" => "用户名不能包含脏话"
@@ -34,5 +33,6 @@ $dtd = array(
  * 初始化Validator，执行验证
  */
 $validator = new LtValidator;
-$result = $validator->validate("fuck my life",$dtd);
+$username = "fuck my life";
+$result = $validator->validate($username, $dtd);
 print_r($result);
