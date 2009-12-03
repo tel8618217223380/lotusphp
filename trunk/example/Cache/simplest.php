8e2c500c843f2db8a88281a1d4197f9ad7f76d11
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-03 10:18:35 +0000

    MVC，还没调试好，先不要测试

--- trunk/example/Cache/simplest.php
+++ trunk/example/Cache/simplest.php
@@ -1,8 +1,9 @@
 <?php
-/* 
- * Load rumtime class start
+/*
+ * 加载Cache类文件
+ * 加载的类很多，且需要注意先后顺序，推荐使用LtAutoloader自动加载
  */
-$lotusHome = dirname(dirname(dirname(dirname(__FILE__))));
+$lotusHome = dirname(dirname(dirname(__FILE__)));
 include $lotusHome . "/runtime/Cache/Cache.php";
 include $lotusHome . "/runtime/Cache/CacheAdapter.php";
 include $lotusHome . "/runtime/Cache/CacheAdapterApc.php";
@@ -13,5 +14,5 @@ include $lotusHome . "/runtime/Cache/CacheAdapterXcache.php";
 
 $cache = new LtCache;
 $cache->init("xcache");
-$cache->add("test_key","hello cache");
+$cache->add("test_key", "hello cache");
 echo $cache->get("test_key");
--- trunk/example/Url/simplest.php
+++ trunk/example/Url/simplest.php
@@ -1,16 +1,14 @@
 ﻿<?php
-/* 
- * Load rumtime class start
+/*
+ * 加载Db类文件
  */
 $lotusHome = dirname(dirname(dirname(__FILE__)));
 include $lotusHome . "/runtime/Url/Url.php";
 include $lotusHome . "/runtime/Url/UrlConfig.php";
-/* 
- * Load rumtime class end
+/*
+ * 加载Db类文件
  */
 
 $url = new LtUrl;
 $url->conf->patern = "rewrite";
 echo $url->generate("Index", "Add");
-//$url->link(__LINE__);
-//$url->link(__LINE__);
