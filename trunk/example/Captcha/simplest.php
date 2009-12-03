e7e7023569023d79aa66bbf62de26b057d023170
tianyi@gmail.com <tianyi@gmail.com@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-03 09:51:12 +0000

    add Captcha

---
+++ trunk/example/Captcha/simplest.php
@@ -0,0 +1,27 @@
+﻿<?php
+/* 
+ * 加载Captcha类文件
+ */
+$lotusHome = dirname(dirname(dirname(__FILE__)));
+include $lotusHome . "/runtime/Captcha/Captcha.php";
+include $lotusHome . "/runtime/Captcha/CaptchaConfig.php";
+/* 
+ * 加载Captcha类文件
+ */
+
+/*
+ * 开始使用Captcha
+ */
+//构造设置Captcha的参数
+
+$fonts = dirname(__FILE__) . "/fonts";
+//$fonts = "D:\AppServ\www\lotus\example\Captcha\fonts";
+
+$captcha = new LtCaptcha();
+$captcha->conf->secretKey = "lotusphp";
+$captcha->conf->length = 5;
+$captcha->conf->fontDir = $fonts;
+
+$seed = uniqid();
+$word = $captcha->getWord($seed);
+$captcha->generateImage($word);
