a317092bca84f0545127bd9d94997c9871015724
tianyi@gmail.com <tianyi@gmail.com@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-03 07:53:31 +0000

    add simplest.php

---
+++ trunk/example/Url/simplest.php
@@ -0,0 +1,16 @@
+﻿<?php
+/* 
+ * Load rumtime class start
+ */
+$lotusHome = dirname(dirname(dirname(__FILE__)));
+include $lotusHome . "/runtime/Url/Url.php";
+include $lotusHome . "/runtime/Url/UrlConfig.php";
+/* 
+ * Load rumtime class end
+ */
+
+$url = new LtUrl;
+$url->conf->patern = "rewrite";
+echo $url->generate("Index", "Add");
+//$url->link(__LINE__);
+//$url->link(__LINE__);
