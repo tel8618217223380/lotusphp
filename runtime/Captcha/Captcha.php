eb5824dfbe76005c79d5c7a33444ea69f386ecce
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-04 13:31:03 +0000

    高性能版的captcha

--- runtime/Captcha/Captcha.php
+++ runtime/Captcha/Captcha.php
@@ -25,212 +25,232 @@ class LtCaptcha
 		return $captchaWord;
 	}
 
-	/*
-	 * @todo performance tuning
-	 * @param $word
-	 * @return unknown_type
+	/** Width of the image */
+	public $width  = 200;
+
+	/** Height of the image */
+	public $height = 70;
+
+	public $maxWordLength = 9;
+
+	/** Background color in RGB-array */
+	public $backgroundColor = array(255, 255, 255);
+
+	/** Foreground colors in RGB-array */
+	public $colors = array(
+	array(27,78,181), // blue
+	array(22,163,35), // green
+	array(214,36,7),  // red
+	);
+
+	/** Shadow color in RGB-array or null */
+	public $shadowColor = null; //array(0, 0, 0);
+
+	/**
+	 * Font configuration
+	 *
+	 * - font: TTF file
+	 * - spacing: relative pixel space between character
+	 * - minSize: min font size
+	 * - maxSize: max font size
 	 */
-	public function generateImage($word)
+	public $fonts = array(
+        'Antykwa'  => array('spacing' => -3, 'minSize' => 27, 'maxSize' => 30, 'font' => 'AntykwaBold.ttf'),
+        'Candice'  => array('spacing' =>-1.5,'minSize' => 28, 'maxSize' => 31, 'font' => 'Candice.ttf'),
+        'DingDong' => array('spacing' => -2, 'minSize' => 24, 'maxSize' => 30, 'font' => 'Ding-DongDaddyO.ttf'),
+        'Duality'  => array('spacing' => -2, 'minSize' => 30, 'maxSize' => 38, 'font' => 'Duality.ttf'),
+        'Heineken' => array('spacing' => -2, 'minSize' => 24, 'maxSize' => 34, 'font' => 'Heineken.ttf'),
+        'Jura'     => array('spacing' => -2, 'minSize' => 28, 'maxSize' => 32, 'font' => 'Jura.ttf'),
+        'StayPuft' => array('spacing' =>-1.5,'minSize' => 28, 'maxSize' => 32, 'font' => 'StayPuft.ttf'),
+        'Times'    => array('spacing' => -2, 'minSize' => 28, 'maxSize' => 34, 'font' => 'TimesNewRomanBold.ttf'),
+        'VeraSans' => array('spacing' => -1, 'minSize' => 20, 'maxSize' => 28, 'font' => 'VeraSansBold.ttf'),
+	);
+
+	/** Wave configuracion in X and Y axes */
+	public $Yperiod    = 12;
+	public $Yamplitude = 14;
+	public $Xperiod    = 11;
+	public $Xamplitude = 5;
+
+	/** letter rotation clockwise */
+	public $maxRotation = 8;
+
+	/**
+	 * Internal image size factor (for better image quality)
+	 * 1: low, 2: medium, 3: high
+	 */
+	public $scale = 2;
+
+	/**
+	 * Blur effect for better image quality (but slower image processing).
+	 * Better image results with scale=3
+	 */
+	public $blur = false;
+
+	/** Debug? */
+	public $debug = false;
+
+	/** Image format: jpeg or png */
+	public $imageFormat = 'jpeg';
+
+
+	/** GD image */
+	public $im;
+
+	public function generateImage($text) {
+		$ini = microtime(true);
+
+		/** Initialization */
+		$this->ImageAllocate();
+
+		$fontcfg  = $this->fonts[array_rand($this->fonts)];
+		$this->WriteText($text, $fontcfg);
+
+		/** Transformations */
+		$this->WaveImage();
+		if ($this->blur && function_exists('imagefilter')) {
+			imagefilter($this->im, IMG_FILTER_GAUSSIAN_BLUR);
+		}
+		$this->ReduceImage();
+
+
+		if ($this->debug) {
+			imagestring($this->im, 1, 1, $this->height-8,
+                "$text {$fontcfg['font']} ".round((microtime(true)-$ini)*1000)."ms",
+			$this->GdFgColor
+			);
+		}
+
+		/** Output */
+		$this->WriteImage();
+		$this->Cleanup();
+	}
+
+	/**
+	 * Creates the image resources
+	 */
+	protected function ImageAllocate() {
+		// Cleanup
+		if (!empty($this->im)) {
+			imagedestroy($this->im);
+		}
+
+		$this->im = imagecreatetruecolor($this->width*$this->scale, $this->height*$this->scale);
+
+		// Background color
+		$this->GdBgColor = imagecolorallocate($this->im,
+		$this->backgroundColor[0],
+		$this->backgroundColor[1],
+		$this->backgroundColor[2]
+		);
+		imagefilledrectangle($this->im, 0, 0, $this->width*$this->scale, $this->height*$this->scale, $this->GdBgColor);
+
+		// Foreground color
+		$color           = $this->colors[mt_rand(0, sizeof($this->colors)-1)];
+		$this->GdFgColor = imagecolorallocate($this->im, $color[0], $color[1], $color[2]);
+
+		// Shadow color
+		if (!empty($this->shadowColor) && is_array($this->shadowColor) && sizeof($this->shadowColor) >= 3) {
+			$this->GdShadowColor = imagecolorallocate($this->im,
+			$this->shadowColor[0],
+			$this->shadowColor[1],
+			$this->shadowColor[2]
+			);
+		}
+	}
+
+	/**
+	 * Text insertion
+	 */
+	protected function WriteText($text, $fontcfg = array()) {
+		if (empty($fontcfg)) {
+			// Select the font configuration
+			$fontcfg  = $this->fonts[array_rand($this->fonts)];
+		}
+
+		// Full path of font file
+		$fontfile = dirname(__FILE__) . '/fonts/'.$fontcfg['font'];
+
+		/** Increase font-size for shortest words: 9% for each glyp missing */
+		$lettersMissing = $this->maxWordLength-strlen($text);
+		$fontSizefactor = 1+($lettersMissing*0.09);
+
+		// Text generation (char by char)
+		$x      = 20*$this->scale;
+		$y      = round(($this->height*27/40)*$this->scale);
+		$length = strlen($text);
+		for ($i=0; $i<$length; $i++) {
+			$degree   = rand($this->maxRotation*-1, $this->maxRotation);
+			$fontsize = rand($fontcfg['minSize'], $fontcfg['maxSize'])*$this->scale*$fontSizefactor;
+			$letter   = substr($text, $i, 1);
+
+			if ($this->shadowColor) {
+				$coords = imagettftext($this->im, $fontsize, $degree,
+				$x+$this->scale, $y+$this->scale,
+				$this->GdShadowColor, $fontfile, $letter);
+			}
+			$coords = imagettftext($this->im, $fontsize, $degree,
+			$x, $y,
+			$this->GdFgColor, $fontfile, $letter);
+			$x += ($coords[2]-$x) + ($fontcfg['spacing']*$this->scale);
+		}
+	}
+
+	/**
+	 * Wave filter
+	 */
+	protected function WaveImage() {
+		// X-axis wave generation
+		$xp = $this->scale*$this->Xperiod*rand(1,3);
+		$k = rand(0, 100);
+		for ($i = 0; $i < ($this->width*$this->scale); $i++) {
+			imagecopy($this->im, $this->im,
+			$i-1, sin($k+$i/$xp) * ($this->scale*$this->Xamplitude),
+			$i, 0, 1, $this->height*$this->scale);
+		}
+
+		// Y-axis wave generation
+		$k = rand(0, 100);
+		$yp = $this->scale*$this->Yperiod*rand(1,2);
+		for ($i = 0; $i < ($this->height*$this->scale); $i++) {
+			imagecopy($this->im, $this->im,
+			sin($k+$i/$yp) * ($this->scale*$this->Yamplitude), $i-1,
+			0, $i, $this->width*$this->scale, 1);
+		}
+	}
+
+	/**
+	 * Reduce the image to the final size
+	 */
+	protected function ReduceImage() 
 	{
-		$alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"; # do not change without changing font files!
+		$imResampled = imagecreatetruecolor($this->width, $this->height);
+		imagecopyresampled($imResampled, $this->im,
+		0, 0, 0, 0,
+		$this->width, $this->height,
+		$this->width*$this->scale, $this->height*$this->scale
+		);
+		imagedestroy($this->im);
+		$this->im = $imResampled;
+	}
 
-		# folder with fonts
-		$fontsdir = $this->conf->fontDir;
-
-		# CAPTCHA image size (you do not need to change it, whis parameters is optimal)
-		$width = isset($this->conf->width) ? $this->conf->width : 120;
-		$height = isset($this->conf->height) ? $this->conf->height : 60;
-
-		# symbol's vertical fluctuation amplitude divided by 2
-		$fluctuation_amplitude = 5;
-
-		# increase safety by prevention of spaces between symbols
-		$no_spaces = true;
-
-		# CAPTCHA image colors (RGB, 0-255)
-		//$foreground_color = array(0, 0, 0);
-		//$background_color = array(220, 230, 255);
-		$foreground_color = array(mt_rand(0,100), mt_rand(0,100), mt_rand(0,100));
-		$background_color = array(mt_rand(200,255), mt_rand(200,255), mt_rand(200,255));
-
-		# JPEG quality of CAPTCHA image (bigger is better quality, but larger file size)
-		$jpeg_quality = 90;
-		$fonts=array();
-		if ($handle = opendir($fontsdir)) {
-			while (false !== ($file = readdir($handle))) {
-				if (preg_match('/\.png$/i', $file)) {
-					$fonts[]=$fontsdir.'/'.$file;
-				}
-			}
-			closedir($handle);
+	/**
+	 * File generation
+	 */
+	protected function WriteImage() {
+		if ($this->imageFormat == 'png' && function_exists('imagepng')) {
+			header("Content-type: image/png");
+			imagepng($this->im);
+		} else {
+			header("Content-type: image/jpeg");
+			imagejpeg($this->im, null, 80);
 		}
+	}
 
-		$alphabet_length=strlen($alphabet);
-
-		do{
-			$font_file=$fonts[mt_rand(0, count($fonts)-1)];
-			$font=imagecreatefrompng($font_file);
-			imagealphablending($font, true);
-			$fontfile_width=imagesx($font);
-			$fontfile_height=imagesy($font)-1;
-			$font_metrics=array();
-			$symbol=0;
-			$reading_symbol=false;
-
-			// loading font
-			for($i=0;$i<$fontfile_width && $symbol<$alphabet_length;$i++){
-				$transparent = (imagecolorat($font, $i, 0) >> 24) == 127;
-
-				if(!$reading_symbol && !$transparent){
-					$font_metrics[$alphabet{$symbol}]=array('start'=>$i);
-					$reading_symbol=true;
-					continue;
-				}
-
-				if($reading_symbol && $transparent){
-					$font_metrics[$alphabet{$symbol}]['end']=$i;
-					$reading_symbol=false;
-					$symbol++;
-					continue;
-				}
-			}
-
-			$img=imagecreatetruecolor($width, $height);
-			imagealphablending($img, true);
-			$white=imagecolorallocate($img, 255, 255, 255);
-			$black=imagecolorallocate($img, 0, 0, 0);
-
-			imagefilledrectangle($img, 0, 0, $width-1, $height-1, $white);
-
-			// draw text
-			$x=1;
-			for($i=0;$i<strlen($word);$i++){
-				$m=$font_metrics[$word{$i}];
-
-				$y=mt_rand(-$fluctuation_amplitude, $fluctuation_amplitude)+($height-$fontfile_height)/2+2;
-
-				if($no_spaces){
-					$shift=0;
-					if($i>0){
-						$shift=10000;
-						for($sy=7;$sy<$fontfile_height-20;$sy+=1){
-							for($sx=$m['start']-1;$sx<$m['end'];$sx+=1){
-								$rgb=imagecolorat($font, $sx, $sy);
-								$opacity=$rgb>>24;
-								if($opacity<127){
-									$left=$sx-$m['start']+$x;
-									$py=$sy+$y;
-									if($py>$height) break;
-									for($px=min($left,$width-1);$px>$left-12 && $px>=0;$px-=1){
-										$color=imagecolorat($img, $px, $py) & 0xff;
-										if($color+$opacity<190){
-											if($shift>$left-$px){
-												$shift=$left-$px;
-											}
-											break;
-										}
-									}
-									break;
-								}
-							}
-						}
-						if($shift==10000){
-							$shift=mt_rand(4,6);
-						}
-
-					}
-				}else{
-					$shift=1;
-				}
-				imagecopy($img, $font, $x-$shift, $y, $m['start'], 1, $m['end']-$m['start'], $fontfile_height);
-				$x+=$m['end']-$m['start']-$shift;
-			}
-		}while($x>=$width-10); // while not fit in canvas
-
-		$center=$x/2;
-
-		// credits. To remove, see configuration file
-		$img2=imagecreatetruecolor($width, $height);
-		$foreground=imagecolorallocate($img2, $foreground_color[0], $foreground_color[1], $foreground_color[2]);
-		$background=imagecolorallocate($img2, $background_color[0], $background_color[1], $background_color[2]);
-		imagefilledrectangle($img2, 0, 0, $width-1, $height-1, $background);
-		imagefilledrectangle($img2, 0, $height, $width-1, $height+12, $foreground);
-
-		// periods
-		$rand1=mt_rand(750000,1200000)/10000000;
-		$rand2=mt_rand(750000,1200000)/10000000;
-		$rand3=mt_rand(750000,1200000)/10000000;
-		$rand4=mt_rand(750000,1200000)/10000000;
-		// phases
-		$rand5=mt_rand(0,31415926)/10000000;
-		$rand6=mt_rand(0,31415926)/10000000;
-		$rand7=mt_rand(0,31415926)/10000000;
-		$rand8=mt_rand(0,31415926)/10000000;
-		// amplitudes
-		$rand9=mt_rand(330,420)/110;
-		$rand10=mt_rand(330,450)/110;
-
-		//wave distortion
-
-		for($x=0;$x<$width;$x++){
-			for($y=0;$y<$height;$y++){
-				$sx=$x+(sin($x*$rand1+$rand5)+sin($y*$rand3+$rand6))*$rand9-$width/2+$center+1;
-				$sy=$y+(sin($x*$rand2+$rand7)+sin($y*$rand4+$rand8))*$rand10;
-
-				if($sx<0 || $sy<0 || $sx>=$width-1 || $sy>=$height-1){
-					continue;
-				}else{
-					$color=imagecolorat($img, $sx, $sy) & 0xFF;
-					$color_x=imagecolorat($img, $sx+1, $sy) & 0xFF;
-					$color_y=imagecolorat($img, $sx, $sy+1) & 0xFF;
-					$color_xy=imagecolorat($img, $sx+1, $sy+1) & 0xFF;
-				}
-
-				if($color==255 && $color_x==255 && $color_y==255 && $color_xy==255){
-					continue;
-				}else if($color==0 && $color_x==0 && $color_y==0 && $color_xy==0){
-					$newred=$foreground_color[0];
-					$newgreen=$foreground_color[1];
-					$newblue=$foreground_color[2];
-				}else{
-					$frsx=$sx-floor($sx);
-					$frsy=$sy-floor($sy);
-					$frsx1=1-$frsx;
-					$frsy1=1-$frsy;
-
-					$newcolor=(
-					$color*$frsx1*$frsy1+
-					$color_x*$frsx*$frsy1+
-					$color_y*$frsx1*$frsy+
-					$color_xy*$frsx*$frsy);
-
-					if($newcolor>255) $newcolor=255;
-					$newcolor=$newcolor/255;
-					$newcolor0=1-$newcolor;
-
-					$newred=$newcolor0*$foreground_color[0]+$newcolor*$background_color[0];
-					$newgreen=$newcolor0*$foreground_color[1]+$newcolor*$background_color[1];
-					$newblue=$newcolor0*$foreground_color[2]+$newcolor*$background_color[2];
-				}
-
-				imagesetpixel($img2, $x, $y, imagecolorallocate($img2, $newred, $newgreen, $newblue));
-			}
-		}
-
-		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-		header('Cache-Control: no-store, no-cache, must-revalidate');
-		header('Cache-Control: post-check=0, pre-check=0', FALSE);
-		header('Pragma: no-cache');
-
-		if(function_exists("imagejpeg")){
-			header("Content-Type: image/jpeg");
-			imagejpeg($img2, null, $jpeg_quality);
-		}else if(function_exists("imagegif")){
-			header("Content-Type: image/gif");
-			imagegif($img2);
-		}else if(function_exists("imagepng")){
-			header("Content-Type: image/x-png");
-			imagepng($img2);
-		}
+	/**
+	 * Cleanup
+	 */
+	protected function Cleanup() {
+		imagedestroy($this->im);
 	}
 }
