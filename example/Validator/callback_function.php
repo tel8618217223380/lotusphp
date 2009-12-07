5ad8a8540e3f215eafafd181b58eb68f03f79ab5
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-07 02:24:04 +0000

    修正代码格式规范为kiwiphp约定

--- example/Validator/callback_function.php
+++ example/Validator/callback_function.php
@@ -1,45 +1,46 @@
 <?php
-/* 
- * 加载Validator类文件
- */
+/**
+* 加载Validator类文件
+*/
 $lotusHome = dirname(dirname(dirname(__FILE__)));
 include $lotusHome . "/runtime/Validator/Validator.php";
 include $lotusHome . "/runtime/Validator/ValidatorConfig.php";
 include $lotusHome . "/runtime/Validator/ValidatorDtd.php";
-/* 
- * 加载Validator类文件
- */
+/**
+* 加载Validator类文件
+*/
 
-/*
- * 构造验证规则
- */
+/**
+* 构造验证规则
+*/
 $dtd = new LtValidatorDtd("用户名",
-	array(
-		"max_length" => 4,
+	array("max_length" => 4,
 		"mask" => "/^[a-z0-9]+$/i",
 		"ban" => "/fuck/",
-		"callback_check_user"=>''
-	),
+		"callback_check_user" => ''
+		),
 	array(
-		//"max_length" 使用默认的错误消息，在$this->conf->errorMessage里
+		// "max_length" 使用默认的错误消息，在$this->conf->errorMessage里
 		"mask" => "用户名只能由数字或字组成",
 		"ban" => "用户名不能包含脏话",
-		"callback_check_user"=>"函数 用户存在"
-	)
-);
+		"callback_check_user" => "函数 用户存在"
+		)
+	);
 
-function check_user($username){
+function check_user($username)
+{
 	// 可以从数据库查询
-	if('fuck my life'==$username) {
+	if ('fuck my life' == $username)
+	{
 		return false;
 	}
 	return true;
 }
 
-/*
- * 初始化Validator，执行验证
- */
+/**
+* 初始化Validator，执行验证
+*/
 $validator = new LtValidator;
 $username = "fuck my life";
-$result = $validator->validate($username, $dtd);
+$result = $validator -> validate($username, $dtd);
 print_r($result);
--- runtime/Validator/Validator.php
+++ runtime/Validator/Validator.php
@@ -1,105 +1,131 @@
 <?php
-class LtValidator {
+class LtValidator
+{
 	public $conf;
 
-	public function __construct() {
+	public function __construct()
+	{
 		$this -> conf = new LtValidatorConfig();
-	} 
+	}
 
-	protected function _ban($value, $ruleValue) {
+	protected function _ban($value, $ruleValue)
+	{
 		return !preg_match($ruleValue, $value);
-	} 
+	}
 
-	protected function _mask($value, $ruleValue) {
+	protected function _mask($value, $ruleValue)
+	{
 		return preg_match($ruleValue, $value);
-	} 
+	}
 
-	protected function _equal_to($value, $ruleValue) {
+	protected function _equal_to($value, $ruleValue)
+	{
 		return $value === $ruleValue;
-	} 
+	}
 
-	protected function _max_length($value, $ruleValue) {
+	protected function _max_length($value, $ruleValue)
+	{
 		return mb_strlen($value) <= $ruleValue;
-	} 
+	}
 
-	protected function _min_length($value, $ruleValue) {
+	protected function _min_length($value, $ruleValue)
+	{
 		return mb_strlen($value) >= $ruleValue;
-	} 
+	}
 
-	protected function _max_value($value, $ruleValue) {
+	protected function _max_value($value, $ruleValue)
+	{
 		return $value <= $ruleValue;
-	} 
+	}
 
-	protected function _min_value($value, $ruleValue) {
+	protected function _min_value($value, $ruleValue)
+	{
 		return $value >= $ruleValue;
-	} 
+	}
 
-	protected function _min_selected($value, $ruleValue) {
+	protected function _min_selected($value, $ruleValue)
+	{
 		return count($value) >= $ruleValue;
-	} 
+	}
 
-	protected function _max_selected($value, $ruleValue) {
+	protected function _max_selected($value, $ruleValue)
+	{
 		return count($value) <= $ruleValue;
-	} 
+	}
 
-	protected function _required($value, $ruleValue) {
-		if (false == $ruleValue) {
+	protected function _required($value, $ruleValue)
+	{
+		if (false == $ruleValue)
+		{
 			return true;
-		} else {
+		}
+		else
+		{
 			return is_array($value) && count($value) || strlen($value);
-		} 
-	} 
+		}
+	}
 
 	/**
-	 * Validate an element
-	 * 
-	 * @param mixed $value 
-	 * @param array $dtd 
-	 * @return array 
-	 */
-	public function validate($value, $dtd) {
+	* Validate an element
+	*
+	* @param mixed $value
+	* @param array $dtd
+	* @return array
+	*/
+	public function validate($value, $dtd)
+	{
 		$errorMessages = array();
 		$label = $dtd -> label;
 
-		if (is_array($dtd -> rules) && count($dtd -> rules)) {
+		if (is_array($dtd -> rules) && count($dtd -> rules))
+		{
 			$messages = isset($dtd -> messages) ? $dtd -> messages : array();
-			foreach ($dtd -> rules as $key => $val) {
+			foreach ($dtd -> rules as $key => $val)
+			{
 				// callback_user_function
-				if ('callback_' == substr($key, 0, 9)) {
-					$method = substr($key, 9); 
+				if ('callback_' == substr($key, 0, 9))
+				{
+					$method = substr($key, 9);
 					// 定义了过程函数
-					if (function_exists($method)) {
-						if (!$method($value, $dtd -> rules[$key])) {
+					if (function_exists($method))
+					{
+						if (!$method($value, $dtd -> rules[$key]))
+						{
 							$errorMessages[$key] = sprintf((isset($messages[$key]) && strlen($messages[$key]) ? $messages[$key] : $this -> conf -> errorMessage[$key]), $label, $dtd -> rules[$key]);
-						} 
+						}
 						continue;
-					} 
+					}
 					// 定义了类方法
 					$rc = new ReflectionClass($val);
-					if ($rc -> hasMethod($method)) {
+					if ($rc -> hasMethod($method))
+					{
 						$rcMethod = $rc -> getMethod($method);
-
-						if ($rcMethod -> isStatic()) {
+						if ($rcMethod -> isStatic())
+						{
 							$ret = $rcMethod -> invoke(null, $value, $dtd -> rules[$key]);
-						} else {
+						}
+						else
+						{
 							// 非静态方法需要一个实例 有待考虑单例
 							$rcInstance = $rc -> newInstance();
 							$ret = $rcMethod -> invoke($rcInstance, $value, $dtd -> rules[$key]);
-						} 
-						if (!$ret) {
+						}
+						if (!$ret)
+						{
 							$errorMessages[$key] = sprintf((isset($messages[$key]) && strlen($messages[$key]) ? $messages[$key] : $this -> conf -> errorMessage[$key]), $label, $dtd -> rules[$key]);
-						} 
+						}
 						continue;
-					} 
+					}
 					continue;
-				} 
+				}
 				// end callback_user_function
 				$validateFunction = '_' . $key;
-				if ((is_bool($dtd -> rules[$key]) || 0 < strlen($dtd -> rules[$key])) && !$this -> $validateFunction($value, $dtd -> rules[$key])) {
+				if ((is_bool($dtd -> rules[$key]) || 0 < strlen($dtd -> rules[$key])) && !$this -> $validateFunction($value, $dtd -> rules[$key]))
+				{
 					$errorMessages[$key] = sprintf((isset($messages[$key]) && strlen($messages[$key]) ? $messages[$key] : $this -> conf -> errorMessage[$key]), $label, $dtd -> rules[$key]);
-				} 
-			} 
-		} 
+				}
+			}
+		}
 		return $errorMessages;
-	} 
-} 
+	}
+}
