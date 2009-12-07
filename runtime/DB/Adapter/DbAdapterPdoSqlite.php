f01f4bd74f80635e1ef55a7b3c5cd342be8498f0
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-07 05:56:59 +0000

    格式化代码为kiwiphp约定

--- runtime/DB/Adapter/DbAdapterPdoSqlite.php
+++ runtime/DB/Adapter/DbAdapterPdoSqlite.php
@@ -1,98 +1,107 @@
 <?php
 /**
- * Database adapter PDO sqlite class
- */
-class LtDbAdapterPdoSqlite extends LtDbAdapterPdo {
+* Database adapter PDO sqlite class
+*/
+class LtDbAdapterPdoSqlite extends LtDbAdapterPdo 
+{
 	/**
-	 * The default database configuration for sqlite
-	 * 
-	 * @var array 
-	 */
+	* The default database configuration for sqlite
+	* 
+	* @var array 
+	*/
 	protected $_config = array('port' => '', 'username' => '', 'password' => '');
 
 	/**
-	 * The PDO construct options
-	 * 
-	 * @var array 
-	 */
+	* The PDO construct options
+	* 
+	* @var array 
+	*/
 	protected $_options = array(PDO :: ATTR_PERSISTENT => false);
 
 	/**
-	 * Create a PDO DSN for the adapter
-	 * 
-	 * @param array $config 
-	 * @return string 
-	 */
-	protected function _dsn($config) {
+	* Create a PDO DSN for the adapter
+	* 
+	* @param array $config 
+	* @return string 
+	*/
+	protected function _dsn($config)
+	{ 
 		// sqlite:/opt/databases/mydb.sq3
 		// sqlite::memory:
 		// sqlite2:/opt/databases/mydb.sq2
 		// sqlite2::memory:
 		$config['dbver'] = isset($config['dbver']) ? $config['dbver'] : 'sqlite';
 		return $config['dbver'] . ':' . $config['host'] . $config['dbname'];
-	} 
+	}
 
 	/**
-	 * Get current db configuration
-	 * 
-	 * @param string $group 
-	 * @param string $node 
-	 * @param string $role 
-	 * @param string $host 
-	 * @return array 
-	 */
-	protected function _getConfig($group, $node, $role = 'master', $host = null) {
+	* Get current db configuration
+	* 
+	* @param string $group 
+	* @param string $node 
+	* @param string $role 
+	* @param string $host 
+	* @return array 
+	*/
+	protected function _getConfig($group, $node, $role = 'master', $host = null)
+	{
 		return $this -> _getBasicConfig($group, $node, $role, $host);
-	} 
+	}
 
 	/**
-	 * Change current schema
-	 * 
-	 * @return void 
-	 */
-	protected function _useSchema() {
-	} 
+	* Change current schema
+	* 
+	* @return void 
+	*/
+	protected function _useSchema()
+	{
+	}
 
 	/**
-	 * Add an adapter-specific LIMIT clause to the SELECT statement.
-	 * 
-	 * @param string $sql 
-	 * @param integer $limit 
-	 * @param integer $offset 
-	 * @return string 
-	 */
-	public function limit($sql, $limit, $offset) {
-		if ($limit > 0) {
+	* Add an adapter-specific LIMIT clause to the SELECT statement.
+	* 
+	* @param string $sql 
+	* @param integer $limit 
+	* @param integer $offset 
+	* @return string 
+	*/
+	public function limit($sql, $limit, $offset)
+	{
+		if ($limit > 0) 
+		{
 			$offset = 0 < $offset ? $offset : 0;
 			$sql .= " LIMIT $limit OFFSET $offset";
-		} 
+		}
 		return $sql;
-	} 
+	}
 
 	/**
-	 * Set encoding for a database connection.
-	 * 
-	 * @param string $encoding 
-	 * @param resource $connection 
-	 * @return void 
-	 */
-	public function setCharset($charset, $connection) {
+	* Set encoding for a database connection.
+	* 
+	* @param string $encoding 
+	* @param resource $connection 
+	* @return void 
+	*/
+	public function setCharset($charset, $connection)
+	{ 
 		// $sql = 'PRAGMA encoding = "' . $charset . '"';
 		// $connection -> exec($sql);
-	} 
+	}
 
 	/**
-	 * Return the column descriptions for a table.
-	 * 
-	 * @param string $table 
-	 * @return array 
-	 */
-	public function showFields($table) {
+	* Return the column descriptions for a table.
+	* 
+	* @param string $table 
+	* @return array 
+	*/
+	public function showFields($table)
+	{
 		$sql = "PRAGMA table_info('" . $table . "')";
 		$queryResult = $this -> query($sql);
 		$result = $queryResult['rows'];
 		$fields = array();
-		foreach ($result as $key => $value) {
+		foreach ($result as $key => $value) 
+		{
 			// ×Ö¶ÎÃû
 			$fields[$value['name']]['name'] = $value['name']; 
 			// ×Ö¶ÎÀàÐÍ
@@ -101,27 +110,30 @@ class LtDbAdapterPdoSqlite extends LtDbAdapterPdo {
 			$precision = null;
 			$scale = null;
 
-			if (preg_match('/^([^\(]+)\(\s*(\d+)\s*,\s*(\d+)\s*\)$/',
-					$fulltype, $matches)) {
+			if (preg_match('/^([^\(]+)\(\s*(\d+)\s*,\s*(\d+)\s*\)$/',$fulltype, $matches)) 
+			{
 				$type = $matches[1];
 				$precision = $matches[2];
 				$scale = $matches[3]; // aka precision
-			} elseif (preg_match('/^([^\(]+)\(\s*(\d+)\s*\)$/',
-					$fulltype, $matches)) {
+			}
+			elseif (preg_match('/^([^\(]+)\(\s*(\d+)\s*\)$/',$fulltype, $matches)) 
+			{
 				$type = $matches[1];
 				$size = $matches[2];
-			} else {
+			}
+			else 
+			{
 				$type = $fulltype;
-			} 
+			}
 
 			$fields[$value['name']]['type'] = $type;
 			/**
-			 * not null is 99, null is 0
-			 */
+			* not null is 99, null is 0
+			*/
 			$fields[$value['name']]['notnull'] = (bool) ($value['notnull'] != 0);
 			$fields[$value['name']]['default'] = $value['dflt_value'];
 			$fields[$value['name']]['primary'] = (bool) ($value['pk'] == 1 && strtoupper($fulltype) == 'INTEGER');
-		} 
+		}
 		return $fields;
-	} 
-} 
+	}
+}
