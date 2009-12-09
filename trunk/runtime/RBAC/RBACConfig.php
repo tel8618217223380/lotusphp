19b1c6fd341c05bacb267503729d9cf0088ac0f0
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-09 04:14:32 +0000

    未完成,仅供讨论.

--- trunk/runtime/RBAC/RBACConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-class LtRBACConfig
-{
-	public $role = array('Administrator' => "管理员", 'User' => "用户", 'Guest' => "来宾");
-	
-	// accessControl
-	public $acl = array('Administrator' => array('*'), 'User'=> array("Index/Index", "User/*"), 'Guest' => array("Index/Index", "User/View"), 'Demo' => array("*/*"));
-}
