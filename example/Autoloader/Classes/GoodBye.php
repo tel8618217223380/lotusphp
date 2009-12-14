2c4d24f58c0ea30c985fd4afb428b068f52df338
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-14 16:14:16 +0000

    更新示例代码的编码为UTF-8

--- example/Autoloader/Classes/GoodBye.php
+++ example/Autoloader/Classes/GoodBye.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * 这个文件不会加载
+ * 因为GoodBye类没有被示例代码用到
+ */
 class GoodBye
 {
 	public function sayBye()
--- example/Autoloader/Classes/HelloWorld.php
+++ example/Autoloader/Classes/HelloWorld.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * 这个文件定义了类
+ * 当示例代码中new HelloWorld()的时候，这个文件会被自动加载
+ */
 class HelloWorld
 {
 	public function sayHello()
