7882e3504cfeb6bee0dbe021f6e0d4a974ed0b27
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-14 09:28:21 +0000

    更新DB和Lotus示例

--- trunk/example/DB/simlest.php
+++ trunk/example/DB/simlest.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * 加载Db类文件
  * 加载的类很多，且需要注意先后顺序，推荐使用LtAutoloader自动加载
  */
@@ -11,7 +11,7 @@ include $lotusHome . "/runtime/DB/Adapter/DbAdapterPdo.php";
 include $lotusHome . "/runtime/DB/Adapter/DbAdapterPdoMysql.php";
 include $lotusHome . "/runtime/DB/QueryEngine/DbTable.php";
 
-/*
+/**
  * 配置数据库连接
  * 关键是给Db::$servers变量赋一个数组，这个数组维度比较复杂 ，所以用DbConfigBuilder构建不容易出错
  * 如果你用别的方式（例如从ini或者yaml读取配置）构造一个同样的数组然后赋值给Db::$servers，效果是一样的
@@ -27,13 +27,14 @@ $dbConfigBuilder->addSingleHost(array(
 ));
 LtDb::$servers = $dbConfigBuilder->getServers();
 
-/*
+/**
  * 直接执行执行SQL
+ * 由于PDO::execute()的潜规则，这里三个查询只能分三次执行，不要合并成这样：$dba->query("$sql1; $sql2; $sql3");
  */
 $dba = LtDb::factory("pdoMysql");
-$result = $dba->query("CREATE DATABASE IF NOT EXISTS lotus_db_test;");
-$result = $dba->query("USE lotus_db_test;");
-$result = $dba->query("
+$dba->query("CREATE DATABASE IF NOT EXISTS lotus_db_test;");
+$dba->query("USE lotus_db_test;");
+$dba->query("
 CREATE TABLE IF NOT EXISTS `user` (
 	`user_id` INT NOT NULL AUTO_INCREMENT COMMENT '用户ID',
 	`username` VARCHAR( 20 ) NOT NULL COMMENT '用户名',
@@ -46,9 +47,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 	)
 );
 ");
-print_r($result);
 
-/*
+/**
  * 使用Table Gateway模式操作数据表
  * 使用上面刚刚建立的lotus_db_test库，user表
  */
