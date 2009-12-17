fc2dc552b9b26cb8787b1c76f0c77c4d873c0b70
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-17 23:06:35 +0000

    临时保存重构sqlite驱动代码。修改DbHandler自动匹配驱动。

--- runtime/DB/Adapter/SqlAdapter/DbSqlAdapterSqlite.php
+++ runtime/DB/Adapter/SqlAdapter/DbSqlAdapterSqlite.php
@@ -1,1 +1,43 @@
 <?php
+class LtDbSqlAdapterSqlite extends LtDbSqlAdapter
+{
+	public function limit($limit, $offset)
+	{
+		return " LIMIT $limit OFFSET $offset";
+	}
+	public function setCharset($charset)
+	{
+		// return 'PRAGMA encoding = "' . $charset . '"';
+	}
+	public function setSchema($schema)
+	{
+		//return "USE $schema";
+	}
+
+	public function showSchemas($database)
+	{
+		//return "SHOW DATABASES";
+	}
+	public function getSchemas($queryResult)
+	{
+		
+	}
+	public function showTables($schema)
+	{
+		// 临时表及其索引不在 SQLITE_MASTER 表中而在 SQLITE_TEMP_MASTER 中出现
+		return "SELECT name FROM sqlite_master WHERE type='table' UNION ALL SELECT name FROM sqlite_temp_master WHERE type='table' ORDER BY name";
+	}
+	public function getTables($queryResult)
+	{
+		
+	}
+	public function showFields($table)
+	{
+		return "PRAGMA table_info('" . $table . "')";
+
+	}
+	public function getFields($queryResult)
+	{
+		
+	}
+}
