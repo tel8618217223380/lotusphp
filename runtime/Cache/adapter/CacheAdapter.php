b2d736f661f88e80acc3403e818453736113271e
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2009-12-25 09:08:53 +0000

    cache草案

--- runtime/Cache/adapter/CacheAdapter.php
+++ runtime/Cache/adapter/CacheAdapter.php
@@ -4,8 +4,6 @@ abstract class LtCacheAdapter
 	public $options;
 
 	abstract public function add($key, $value, $ttl=0);
-	
 	abstract public function del($key);
-	
 	abstract public function get($key);
 }
