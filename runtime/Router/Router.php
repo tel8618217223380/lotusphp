0845e9f0d8f849ee4dccd6a4e35c9c2862667671
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-04 09:52:30 +0000

    临时保存

--- runtime/Router/Router.php
+++ runtime/Router/Router.php
@@ -1,7 +1,7 @@
 <?php
 /**
-* The Router class
-*/
+ * The Router class
+ */
 class LtRouter
 {
 	public $module;
@@ -13,12 +13,53 @@ class LtRouter
 		$this->conf = new LtRouterConfig;
 	}
 
+	public function matchingRoutingTable()
+	{
+		$ret = array();
+		$url = "news/list/catid/4/page/10";
+		$routingTable['pattern'] = "{module}/{action}/{*}";
+//		$routingTable['config'] = array(
+//			'module' => '([a-z][a-z0-9]*)*',
+//			'action' => '[a-z][a-z0-9]*)*',
+//			);
+//		$routingTable['defaltVar'] = array(
+//			'module' => 'default',
+//			'action' => 'index',
+//			);
+		$pattern = $routingTable['pattern'];
+
+		preg_match_all("/\{([a-z]+|\*)\}/", $pattern, $out);
+		foreach($out[1] as $k => $v)
+		{
+			if ('*' != $v)
+			{
+				if (isset($url[$k]))
+				{ 
+					// $regex = "/^{$routingTable['config'][$v]}\$/i";
+					// if (preg_match($regex, $url[$k]))
+					// {
+					$ret[$v] = $url[$k]; 
+					// }
+				}
+			}
+			else
+			{
+				while (isset($url[$k]))
+				{
+					$ret[$url[$k ++]] = $url[$k];
+					$k++;
+				}
+			}
+		}
+		return $ret;
+	}
+
 	public function init()
 	{
 		$module = '';
 		$action = '';
-		if (isset($_SERVER['SERVER_PROTOCOL']))
-		{	// index.php?module=module&action=action
+		if (isset($_SERVER['SERVER_PROTOCOL'])) // index.php?module=module&action=action
+		{
 			if (isset($_REQUEST['module']))
 			{
 				$module = $_REQUEST['module'];
@@ -26,15 +67,15 @@ class LtRouter
 			if (isset($_REQUEST['action']))
 			{
 				$action = $_REQUEST['action'];
-			}
+			} 
 			// index.php/module/action/
 			if (empty($module) && empty($action) && isset($_SERVER['PATH_INFO']))
 			{
-				list($module,$action) = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+				list($module, $action) = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 			}
 		}
-		else
-		{	// CLI模式
+		else // CLI模式
+		{
 			$i = 0;
 			while ((empty($module) || empty($action)) && isset($_SERVER['argv'][$i]))
 			{
@@ -48,7 +89,7 @@ class LtRouter
 				}
 				$i ++;
 			}
-		}
+		} 
 		// module名字只能大小写字母 数字 . - _
 		if (!empty($module) && !preg_match('/^[a-zA-Z0-9\.\-_]+$/', $module))
 		{
@@ -60,7 +101,7 @@ class LtRouter
 			{
 				throw new Exception("Module name is illegal: {$module}");
 			}
-		}
+		} 
 		// action 名字只能大小写字母 数字 . - _
 		if (!empty($action) && !preg_match('/^[a-zA-Z0-9\.\-_]+$/', $action))
 		{
