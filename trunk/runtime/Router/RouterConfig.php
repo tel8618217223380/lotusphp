b143600f4f719a6e228f662b10781fbde38ecb06
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-04 09:52:30 +0000

    临时保存

--- trunk/runtime/Router/RouterConfig.php
+++ trunk/runtime/Router/RouterConfig.php
@@ -1,6 +1,27 @@
 <?php
+// Options +FollowSymLinks
+// IndexIgnore */*
+// ##  Turn on the RewriteEngine
+// RewriteEngine On
+// ##  Rules
+// RewriteCond %{REQUEST_FILENAME} !-f
+// RewriteCond %{REQUEST_FILENAME} !-d
+// RewriteRule . index.php
 class LtRouterConfig
 {
 	public $module = 'Module';
 	public $action = 'Action';
+
+//	public function __construct()
+//	{
+//		$routingTable['pattern'] = "{module}/{action}/{*}";
+//		$routingTable['config'] = array(
+//			'module' => '([a-z][a-z0-9]*)*',
+//			'action' => '[a-z][a-z0-9]*)*',
+//			);
+//		$routingTable['defaltVar'] = array(
+//			'module' => 'default',
+//			'action' => 'index',
+//			);
+//	}
 }
