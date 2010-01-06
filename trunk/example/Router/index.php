1fe962b5a299aa57d5179046f4596eb564649b6e
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-06 03:26:35 +0000

    重写例子

---
+++ trunk/example/Router/index.php
@@ -0,0 +1,21 @@
+﻿﻿﻿<?php
+/**
+ * 加载Cookie类文件
+ */
+$lotusHome = substr(__FILE__, 0, strpos(__FILE__, "example"));
+include $lotusHome . "/runtime/Router/Router.php";
+
+$router = new LtRouter;
+$router->routingTable = array('pattern' => ":module/:action/*",
+	'default' => array('module' => 'default', 'action' => 'index'),
+	'reqs' => array('module' => '[a-zA-Z0-9\.\-_]+', 'action' => '[a-zA-Z0-9\.\-_]+'),
+	'varprefix' => ':',
+	'delimiter' => '/'
+	);
+$router->init();
+
+echo "\n<pre>\n";
+print_r($router);
+echo $router->url($router->params)."\n";
+echo http_build_query($router->params)."\n";
+echo "\n</pre>\n";
