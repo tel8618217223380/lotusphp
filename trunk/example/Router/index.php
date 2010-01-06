7727d63c666534487fb8fd31f47c48d041798743
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-06 04:18:00 +0000

    保存

--- trunk/example/Router/index.php
+++ trunk/example/Router/index.php
@@ -1,20 +1,21 @@
-﻿﻿﻿<?php
+﻿﻿<?php
 /**
  * 加载Cookie类文件
  */
 $lotusHome = substr(__FILE__, 0, strpos(__FILE__, "example"));
 include $lotusHome . "/runtime/Router/Router.php";
-
+//unset($_SERVER["PATH_INFO"]);
 $router = new LtRouter;
-$router->routingTable = array('pattern' => ":module/:action/*",
+$router->routingTable = array('pattern' => ":module-:action-*",
 	'default' => array('module' => 'default', 'action' => 'index'),
 	'reqs' => array('module' => '[a-zA-Z0-9\.\-_]+', 'action' => '[a-zA-Z0-9\.\-_]+'),
 	'varprefix' => ':',
-	'delimiter' => '/'
+	'delimiter' => '-',
+	'postfix' => '.html'
 	);
 $router->init();
-
-echo "\n<pre>\n";
+echo "<pre>\n";
+echo "输入测试url例如 m-a-id-123.html\n";
 print_r($router);
 echo $router->url($router->params)."\n";
 echo http_build_query($router->params)."\n";
