a84b05223db7ca19b39a2d6a9cc38e3b13b6f489
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-14 07:39:23 +0000

    增加分页组件

--- trunk/unittest/Pagination/RightWayToUse.php
+++ trunk/unittest/Pagination/RightWayToUse.php
@@ -1,32 +1,31 @@
 <?php
 /**
  * 本测试文档演示了LtPagination的正确使用方法 
- * 按本文档操作一定会得到正确的结果 
+ * 按本文档操作一定会得到正确的结果
  */
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "common.inc.php";
-class RightWayToUseCache extends PHPUnit_Framework_TestCase
+class RightWayToUseCache extends PHPUnit_Extensions_OutputTestCase
 {
 	/**
-	 * 最常用的使用方式 
+	 * 最常用的使用方式
 	 */
 	public function testMostUsedWay()
 	{
 		/**
 		 * Lotus组件初始化三步曲
-		 */
+		 */ 
 		// 1. 实例化
-		$pagination = new LtPagination;
+		$pagination = new LtPagination; 
 		// 2. 设置属性
-		$pagination->conf['base_url'] = 'comment/view/page/:page';
-		$pagination->conf['total_rows'] = 586;
-		$pagination->conf['per_page'] = 20;
-		$pagination->conf['uri_segment'] = 4;
+		$pagination->conf->total_rows = 1000; //总数
+		$pagination->conf->cur_page = 5; //当前页
+		$pagination->conf->page_size = 25; //每页显示数
+		$pagination->conf->base_url = 'page.php?page=:page'; // :page会自动被替换掉 
 		// 3. 调init()方法
-		$pagination->init();
-
-		//初始化完毕，测试其效果
-		$pages = $pagination->generate();
-
+		$pagination->init(); 
+		// 初始化完毕，测试其效果
+		echo $pagination->pages;
+		// 判断输出内容是否正确
+		$this->expectOutputString('<div class="pages"><a href="page.php?page=1" style="font-weight:bold">&lsaquo;</a><a href="page.php?page=2">2</a><a href="page.php?page=3">3</a><a href="page.php?page=4">4</a><strong>5</strong><a href="page.php?page=6">6</a><a href="page.php?page=7">7</a><a href="page.php?page=8">8</a><a href="page.php?page=9">9</a><a href="page.php?page=40" style="font-weight:bold">&rsaquo;</a>5/40 goto<input type="text" size="3" onkeydown="javascript: if(event.keyCode==13){ location=\'page.php?page=:page\'.replace(\':page\',this.value);return false;}" />page total 1000</div>');
 	}
-
 }
