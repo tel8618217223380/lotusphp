4bcf83cafeff249f43a097aaf080cee7c9430df7
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-27 06:47:34 +0000

    整理Router和Url组件

--- trunk/runtime/Url/UrlConfig.php
+++ trunk/runtime/Url/UrlConfig.php
@@ -1,7 +1,13 @@
 <?php
 class LtUrlConfig
 {
-	public $bootstrapFile;
-	public $patern;
-	public $prefix;
+	// 默认的路由表 
+	public $routingTable = array('pattern' => ":module/:action/*",
+		'default' => array('module' => 'default', 'action' => 'index'),
+		'reqs' => array('module' => '[a-zA-Z0-9\.\-_]+', 'action' => '[a-zA-Z0-9\.\-_]+'),
+		'varprefix' => ':',
+		'delimiter' => '/',
+		'postfix' => '',
+		'protocol' => 'PATH_INFO', // standard rewrite path_info
+		);
 }
