7b776e77f5ec2b2c0cf635a01f3089c1ab9eedbd
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-27 07:59:45 +0000

    增加mvc中模板引擎url生成的测试用例

--- trunk/unittest/MVC/common.inc.php
+++ trunk/unittest/MVC/common.inc.php
@@ -6,3 +6,33 @@ require_once $lotusHome . "runtime/MVC/Context.php";
 require_once $lotusHome . "runtime/MVC/Dispatcher.php";
 require_once $lotusHome . "runtime/MVC/TemplateView.php";
 require_once $lotusHome . "runtime/MVC/View.php";
+
+
+/**
+ * 用这个类把 LtTemplateView 的 protected 属性和方法暴露出来测试
+ */
+class LtTemplateViewProxy extends LtTemplateView
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function __get($prop)
+	{
+		if (isset($this->$prop))
+		{
+			return $this->$prop;
+		}
+	}
+
+	public function parse($str)
+	{
+		return parent::parse($str);
+	}
+
+	public function addquote($var)
+	{
+		return parent::addquote($var);
+	}
+}
