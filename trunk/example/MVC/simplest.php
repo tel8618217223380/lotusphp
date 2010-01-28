19cd2e164c0eb3d5efd75f6cc6c8ba11347b8550
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-28 08:22:23 +0000

    mvc 补全 Validator

--- trunk/example/MVC/simplest.php
+++ trunk/example/MVC/simplest.php
@@ -9,6 +9,11 @@ include $lotusHome . "/runtime/MVC/Action.php";
 include $lotusHome . "/runtime/MVC/Component.php";
 include $lotusHome . "/runtime/MVC/Context.php";
 include $lotusHome . "/runtime/MVC/View.php";
+
+include $lotusHome . "/runtime/Validator/Validator.php";
+include $lotusHome . "/runtime/Validator/ValidatorConfig.php";
+include $lotusHome . "/runtime/Validator/ValidatorDtd.php";
+
 /**
  * 加载Action类文件
  */
--- trunk/example/MVC/simplest_app/action/UserSigninAction.php
+++ trunk/example/MVC/simplest_app/action/UserSigninAction.php
@@ -1,6 +1,21 @@
 <?php
 class UserSigninAction extends LtAction
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->dtds['username'] = new LtValidatorDtd("username",
+			array("max_length" => 8,
+				"mask" => "/^[a-z0-9]+$/i",
+				"ban" => "/fuck/",
+				),
+			array(
+				// "max_length" 使用默认的错误消息，在$this->conf->errorMessages里
+				"mask" => "用户名只能由数字或字组成",
+				"ban" => "用户名不能包含脏话"
+				)
+			);
+	}
 	/**
 	 * 一定要有这个execute()方法
 	 */
