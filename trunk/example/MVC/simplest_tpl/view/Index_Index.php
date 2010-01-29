8c538cb9e08cdeb564e28e052dbd863d20c45643
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-29 15:40:29 +0000

    修正对include后的文件去掉行首尾空白, html javascript css注释的处理.

--- trunk/example/MVC/simplest_tpl/view/Index_Index.php
+++ trunk/example/MVC/simplest_tpl/view/Index_Index.php
@@ -2,10 +2,9 @@ The <em>index</em> module, <em>Index</em> action
 <h1>{$this->code} - {$this->message}</h1>
 
 <form>
-<input type="text" name="username" value="{if (isset($this->data['username']))}
-{$this->data['username']}{/if}" />
+	<input type="text" name="username" value="{if (isset($this->data['username']))}{$this->data['username']}{/if}" />
 </form>
 
 <pre>
-<?php print_r($this->data);?>
+	<?php print_r($this->data);?>
 </pre>
--- trunk/example/MVC/simplest_tpl/view/test_PassData.php
+++ trunk/example/MVC/simplest_tpl/view/test_PassData.php
@@ -1,15 +1,15 @@
 <fieldset>
-<legend>Data passed by <em>$this-&gt;data</em></legend>
-<xmp>
-<?php print_r($this->data) ?>
-</xmp>
+	<legend>Data passed by <em>$this-&gt;data</em></legend>
+	<xmp>
+	<?php print_r($this->data) ?>
+	</xmp>
 </fieldset>
 
 <fieldset>
-<legend>Data passed by <em>$this-&gt;code</em> <em>$this-&gt;message</em></legend>
-<xmp>
-<?php
-echo "code: $this->code\nmessage: $this->message";
-?>
-</xmp>
+	<legend>Data passed by <em>$this-&gt;code</em> <em>$this-&gt;message</em></legend>
+	<xmp>
+	<?php
+	echo "code: $this->code\nmessage: $this->message";
+	?>
+	</xmp>
 </fieldset>
