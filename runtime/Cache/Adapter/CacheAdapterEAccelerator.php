9a0c61f8f1310f04f9628a7e383cf16ade1a2871
talkativedoggy <talkativedoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-31 15:33:02 +0000

    EA, Xcache改完，但还没测试，没环境，但应该是可以工作的

--- runtime/Cache/Adapter/CacheAdapterEAccelerator.php
+++ runtime/Cache/Adapter/CacheAdapterEAccelerator.php
@@ -1,18 +1,53 @@
 <?php
 class LtCacheAdapterEAccelerator implements LtCacheAdapter
 {
+	protected $keyPrefix;
+
+	public function connect($hostConf)
+	{
+		if(isset($hostConf["key_prefix"]))
+		{
+			$this->keyPrefix = $hotConf["key_prefix"];
+		}
+		return true;
+	}
+
 	public function add($key, $value, $ttl=0)
 	{
-		return eaccelerator_put($key, $value, $ttl);
+		return eaccelerator_put($this->getRealKey($key), $value, $ttl);
 	}
 
 	public function del($key)
 	{
-		return eaccelerator_rm($key);
+		return eaccelerator_rm($this->getRealKey($key));
 	}
 
 	public function get($key)
 	{
-		return eaccelerator_get($key);
+		return eaccelerator_get($this->getRealKey($key));
+	}
+
+	public function update($key, $value, $ttl = 0)
+	{
+		if ($this->del($this->getRealKey($key)))
+		{
+			return $this->add($this->getRealKey($key), $value, $ttl);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	protected function getRealKey($key)
+	{
+		if ($this->keyPrefix)
+		{
+			return $this->keyPrefix . "-" . $key;
+		}
+		else
+		{
+			return $key;
+		}
 	}
 }
--- runtime/Cache/Adapter/CacheAdapterXcache.php
+++ runtime/Cache/Adapter/CacheAdapterXcache.php
@@ -1,18 +1,53 @@
 <?php
 class LtCacheAdapterXcache implements LtCacheAdapter
 {
+	protected $keyPrefix;
+
+	public function connect($hostConf)
+	{
+		if(isset($hostConf["key_prefix"]))
+		{
+			$this->keyPrefix = $hotConf["key_prefix"];
+		}
+		return true;
+	}
+
 	public function add($key, $value, $ttl=0)
 	{
-		return xcache_set($key, $value, $ttl);
+		return xcache_set($this->getRealKey($key), $value, $ttl);
 	}
 	
 	public function del($key)
 	{
-		return xcache_unset($key);
+		return xcache_unset($this->getRealKey($key));
 	}
 	
 	public function get($key)
 	{
-		return xcache_get($key);
+		return xcache_get($this->getRealKey($key));
+	}
+
+	public function update($key, $value, $ttl = 0)
+	{
+		if ($this->del($this->getRealKey($key)))
+		{
+			return $this->add($this->getRealKey($key), $value, $ttl);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	protected function getRealKey($key)
+	{
+		if ($this->keyPrefix)
+		{
+			return $this->keyPrefix . "-" . $key;
+		}
+		else
+		{
+			return $key;
+		}
 	}
 }
