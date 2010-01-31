48b1eddfac52b8825fac1b03423f7480e71aff57
talkativedoggy <talkativedoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-01-31 15:20:24 +0000

    CacheFileAdapter改好了

--- trunk/unittest/Cache/common.inc.php
+++ trunk/unittest/Cache/common.inc.php
@@ -8,6 +8,6 @@ require_once $lotusHome . "runtime/Cache/CacheHandle.php";
 require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapter.php";
 //require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterApc.php";
 //require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterEAccelerator.php";
-//require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterFile.php";
+require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterFile.php";
 require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterPhps.php";
 //require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterXcache.php";
