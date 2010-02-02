c705e0b6c1fb7ce2517816a8acac7f3eabfe896f
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-02-02 09:43:12 +0000

    改为 protected $devMode; 如果设置了cache自动设置生产模式, 否则使用开发模式.

--- trunk/unittest/Lotus/RightWayToUse.php
+++ trunk/unittest/Lotus/RightWayToUse.php
@@ -8,7 +8,7 @@ class RightWayToUseLotus extends PHPUnit_Framework_TestCase
 {
 	/**
 	 * 最常用的使用方式（推荐）
-	 * 
+
 	 * 本测试用例期望效果：
 	 * 能成功通过query()接口存取数据
 	 */
@@ -18,20 +18,24 @@ class RightWayToUseLotus extends PHPUnit_Framework_TestCase
 		 * 初始化Lotus类
 		 */
 		$lotus = new Lotus;
-		
+
 		/**
-		 * devMode的默认值是true，即默认处于开发模式
-		 * devMode等于false的时候（如生产环境，测试环境），性能会有提高
+		 * devMode的默认值是true，即默认处于开发模式 
+		 * devMode等于false的时候（如生产环境，测试环境），性能会有提高 
 		 * $lotus->devMode = false;
+		 * 当指定cache后,自动设置生产模式
+		 * 没有指定cache，自动设置开发模式
 		 */
-		$lotus->devMode = true;
-		//$lotus->option["cache_adapter"] = "file";
-		// 必需指定配置文件
+		$lotus->option["cache"] = array("adapter" => "phps",
+			"host" => "/tmp/cache_lotus/");
+		/**
+		 * 必需指定配置文件
+		 */
 		$lotus->option["config_file"] = dirname(__FILE__) . "/conf.php";
 		$lotus->init();
-		
+
 		/**
-		 * 
+		 * class_exists默认调用自动加载
 		 */
 		$this->asserttrue(class_exists("LtCaptcha"));
 	}
