6c3e9d8170ec7320e4e944aa2c1634cecf9e21c3
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-02-02 16:48:58 +0000

    修正bindParameter()错误, 测试sql如下: $sql = 'INSERT INTO cms_c_news (author,copyfrom,copyfromurl,content,contentid) VALUES (:author,:copyfrom,:copyfromurl,:content,:contentid); SELECT * FROM cms_admin WHERE cms_admin.admin_id=:admin_-id; SELECT * FROM cms_admin WHERE cms_admin.admin_id=:admin_-id';

--- runtime/DB/DbHandle.php
+++ runtime/DB/DbHandle.php
@@ -34,25 +34,26 @@ class LtDbHandle
 
 	/**
 	 * Execute an sql query
-	 * @param $sql
-	 * @param $bind
-	 * @param $forceUseMaster
+	 * 
+	 * @param  $sql 
+	 * @param  $bind 
+	 * @param  $forceUseMaster 
 	 * @return false on query failed
-	 *         --sql type--                         --return value--
-	 *         SELECT, SHOW, DESECRIBE, EXPLAIN     rowset or NULL when no record found
-	 *         INSERT                               the ID generated for an AUTO_INCREMENT column
-	 *         UPDATE, DELETE, REPLACE              affected count
-	 *         USE, DROP, ALTER, CREATE, SET etc    true
+	 *          --sql type--                         --return value--
+	 *          SELECT, SHOW, DESECRIBE, EXPLAIN     rowset or NULL when no record found
+	 *          INSERT                               the ID generated for an AUTO_INCREMENT column
+	 *          UPDATE, DELETE, REPLACE              affected count
+	 *          USE, DROP, ALTER, CREATE, SET etc    true
 	 * @notice 每次只能执行一条SQL
-	 *         不要通过此接口执行USE DATABASE, SET NAMES这样的语句
+	 *          不要通过此接口执行USE DATABASE, SET NAMES这样的语句
 	 */
 	public function query($sql, $bind = null, $forceUseMaster = false)
 	{
-		if(empty($sql))
+		if (empty($sql))
 		{
 			$this->sqlAdapter = $this->getCurrentSqlAdapter();
-			return;
-			//trigger_error('Empty the SQL statement');
+			return; 
+			// trigger_error('Empty the SQL statement');
 		}
 		$this->sqlAdapter = $this->getCurrentSqlAdapter();
 		$queryType = $this->sqlAdapter->detectQueryType($sql);
@@ -91,33 +92,35 @@ class LtDbHandle
 			$sql = $this->bindParameter($sql, $bind);
 		}
 		return $this->$queryMethod($sql, $this->connectionResource);
-    }
-    /**
-     * function posted by renlu 
-     * */
-    public function escape($str){
-       return $this->connectionAdapter->escape($str,$this->connectionResource); 
-    }
-    /**
-     * function posted by renlu
-     * */
-    public function insertid(){
-			return $this->connectionAdapter->lastInsertId($this->connectionResource);
-    }
+	}
+	/**
+	 * function posted by renlu
+	 */
+	public function escape($str)
+	{
+		return $this->connectionAdapter->escape($str, $this->connectionResource);
+	}
+	/**
+	 * function posted by renlu
+	 */
+	public function insertid()
+	{
+		return $this->connectionAdapter->lastInsertId($this->connectionResource);
+	}
 	/**
 	 * Generate complete sql from sql template (with placeholder) and parameter
-	 * @param $sql
-	 * @param $parameter
-	 * @return string
+	 * 
+	 * @param  $sql 
+	 * @param  $parameter 
+	 * @return string 
 	 * @todo 兼容pgsql等其它数据库，pgsql的某些数据类型不接受单引号引起来的值
 	 */
 	public function bindParameter($sql, $parameter)
 	{
-		$delimiter = "\x01\x02\x03";
+		$sql = preg_replace("/:([a-zA-Z0-9_\-\x7f-\xff][a-zA-Z0-9_\-\x7f-\xff]*)\s*([,\)]?)/", "\x01\x02\x03\\1\x01\x02\x03\\2", $sql);
 		foreach($parameter as $key => $value)
 		{
-			$newPlaceHolder = "$delimiter$key$delimiter";
-			$find[] = $newPlaceHolder;
+			$find[] = "\x01\x02\x03$key\x01\x02\x03";
 			if ($value instanceof LtDbSqlExpression)
 			{
 				$replacement[] = $value->__toString();
@@ -126,9 +129,9 @@ class LtDbHandle
 			{
 				$replacement[] = "'" . $this->connectionAdapter->escape($value, $this->connectionResource) . "'";
 			}
-			$sql = str_replace(":$key", $newPlaceHolder, $sql);
 		}
-		return str_replace($find, $replacement, $sql);
+		$sql = str_replace($find, $replacement, $sql);
+		return $sql;
 	}
 
 	protected function getCurrentSqlAdapter()
@@ -154,7 +157,7 @@ class LtDbHandle
 
 	protected function insert($sql, $connResource)
 	{
-		if($result = $this->connectionAdapter->exec($sql, $connResource))
+		if ($result = $this->connectionAdapter->exec($sql, $connResource))
 		{
 			return $this->connectionAdapter->lastInsertId($connResource);
 		}
@@ -168,8 +171,9 @@ class LtDbHandle
 	{
 		return $this->connectionAdapter->exec($sql, $connResource);
 	}
-    
+
 	/**
+	 * 
 	 * @todo 更新连接缓存
 	 */
 	protected function setSessionVar($sql, $connResource)
