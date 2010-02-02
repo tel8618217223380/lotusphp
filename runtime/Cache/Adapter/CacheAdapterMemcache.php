68a56adc71176f7d2bc83edc2d2febb8f67c4c1b
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-02-02 13:02:49 +0000

    php_memcache驱动测试完毕，能正常工作。 php_memcached驱动还没测试，没找到这个dll，估计也能正常工作的

---
+++ runtime/Cache/Adapter/CacheAdapterMemcache.php
@@ -0,0 +1,55 @@
+<?php
+class LtCacheAdapterMemcache implements LtCacheAdapter
+{
+	protected $keyPrefix;
+	protected $connectionResource;
+
+	public function connect($hostConf)
+	{
+		if(isset($hostConf["key_prefix"]))
+		{
+			$this->keyPrefix = $hotConf["key_prefix"];
+		}
+		$this->connectionResource = memcache_connect($hostConf["host"], $hostConf["port"]);
+		return $this->connectionResource;
+	}
+
+	public function add($key, $value, $ttl=0)
+	{
+		return $this->connectionResource->add($this->getRealKey($key), $value, $ttl);
+	}
+
+	public function del($key)
+	{
+		return $this->connectionResource->delete($this->getRealKey($key));
+	}
+
+	public function get($key)
+	{
+		return $this->connectionResource->get($this->getRealKey($key));
+	}
+
+	public function update($key, $value, $ttl = 0)
+	{
+		if ($this->del($this->getRealKey($key)))
+		{
+			return $this->add($this->getRealKey($key), $value, $ttl);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	protected function getRealKey($key)
+	{
+		if ($this->keyPrefix)
+		{
+			return $this->keyPrefix . "-" . $key;
+		}
+		else
+		{
+			return $key;
+		}
+	}
+}
