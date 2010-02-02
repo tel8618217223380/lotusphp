27d1a5429b53ea5a28564f0e21ad9ac7a920692c
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-02-02 06:58:19 +0000

    更新要加载的缓存文件列表，但还没去掉$namespace

--- unittest/DB/PerformanceTuning.php
+++ unittest/DB/PerformanceTuning.php
@@ -1,11 +1,15 @@
 <?php
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "common.inc.php";
 require_once $lotusHome . "runtime/Cache/Cache.php";
-require_once $lotusHome . "runtime/Cache/CacheConfig.php";
+require_once $lotusHome . "runtime/Cache/CacheAdapterFactory.php";
+require_once $lotusHome . "runtime/Cache/CacheConfigBuilder.php";
+require_once $lotusHome . "runtime/Cache/CacheConnectionManager.php";
+require_once $lotusHome . "runtime/Cache/CacheHandle.php";
 require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapter.php";
 require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterApc.php";
 require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterEAccelerator.php";
 require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterFile.php";
+require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterMemcached.php";
 require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterPhps.php";
 require_once $lotusHome . "runtime/Cache/Adapter/CacheAdapterXcache.php";
 class PerformanceTuningDb extends PHPUnit_Framework_TestCase
