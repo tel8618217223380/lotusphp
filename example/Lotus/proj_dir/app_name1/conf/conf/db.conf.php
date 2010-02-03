97b3f565be10bb69e64ee978fe1f419b714ab437
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-02-03 07:43:44 +0000

    数据库测试通过

--- example/Lotus/proj_dir/app_name1/conf/conf/db.conf.php
+++ example/Lotus/proj_dir/app_name1/conf/conf/db.conf.php
@@ -1,5 +1,12 @@
 <?php
 
-$host = dirname(dirname(dirname(__FILE__))) .  DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR;
-$dbname = 'cms.db';
+$host = substr(dirname(__FILE__), 0, strpos(__FILE__, "proj_dir")) . 'db/';
+$dbname = 'sqlite_test0.db';
+
 $config['singleHost'] = array("adapter" => "sqlite", "host" => $host, "port"=>'', "password" => "", "dbname" => $dbname);
+
+$config['db_server'][] = array("user_group", "user_node_1", "master",array("host" => "10.0.1.1", "password" => "123456", "adapter" => "mysqli", "dbname" => "member_1"));
+
+$config['db_server'][] = array("group_1", "node_1", "master",array("adapter" => "sqlite", "host" => $host, "port"=>'', "password" => "", "dbname" => 'sqlite_test1.db'));
+
+$config['db_server'][] = array("group_8", "node_8", "master",array("adapter" => "mysql", "host" => 'localhost', "port"=>'', 'username'=>'root', "password" => "123456", "dbname" => 'test'));
