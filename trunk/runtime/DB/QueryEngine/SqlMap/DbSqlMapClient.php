c339fd949c0e72de2f01b339f7f83d1cd20538fb
talkativedoggy <talkativedoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-03-07 15:53:12 +0000

    cache单元测试修改思路（还没改完）： 1.TTL测试放入TestOpcodeCacheAdapter()和testOtherCacheAdapter()里面去 2.opcode cache adapter的测试单独拿出来，通过http://localhost代理来测试，即，apc_add()这样的调用写在web脚本里，phpunit调用web脚本，然后判断测试是否成功

--- trunk/runtime/DB/QueryEngine/SqlMap/DbSqlMapClient.php
+++ trunk/runtime/DB/QueryEngine/SqlMap/DbSqlMapClient.php
@@ -3,22 +3,10 @@ class LtDbSqlMapClient
 {
 	public $dbh;
 
-	/**
-	 * @todo 实现sql map的存储
-	 */
 	public function execute($mapId, $bind = null)
 	{
-		$testMap = array(
-			"getAgeTotal" => array(
-				"sql" => "SELECT COUNT(DISTINCT age) as age_total FROM test_user",
-				"force_use_master" => true, 
-			),
-			"sys.getSysCateTotal" => array(
-				"sql" => "SELECT COUNT(DISTINCT name) as category_total FROM sys_category",
-				"force_use_master" => true, 
-			)
-		);
-		$forceUseMaster = isset($testMap[$mapId]["force_use_master"]) ? $testMap[$mapId]["force_use_master"] : false;
-		return $this->dbh->query($testMap[$mapId]["sql"], $bind, $forceUseMaster);
+		$sqlMap = LtDb::$storeHandle->get($this->dbh->group . "." . $mapId);
+		$forceUseMaster = isset($sqlMap["force_use_master"]) ? $sqlMap["force_use_master"] : false;
+		return $this->dbh->query($sqlMap["sql"], $bind, $forceUseMaster);
 	}
 }
