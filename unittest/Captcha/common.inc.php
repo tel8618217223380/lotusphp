854c663660900cc06fa2971d9afbb1186b06341f
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-03-24 08:28:58 +0000

    LtStore文件重命名，去年Lt前缀

--- unittest/Captcha/common.inc.php
+++ unittest/Captcha/common.inc.php
@@ -1,7 +1,7 @@
 <?php
 $lotusHome = substr(__FILE__, 0, strpos(__FILE__, "unittest"));
-require_once $lotusHome . "runtime/LtStore.php";
-require_once $lotusHome . "runtime/LtStoreFile.php";
+require_once $lotusHome . "runtime/Store.php";
+require_once $lotusHome . "runtime/StoreFile.php";
 require_once $lotusHome . "runtime/Captcha/Captcha.php";
 require_once $lotusHome . "runtime/Captcha/CaptchaConfig.php";
 require_once $lotusHome . "runtime/Captcha/CaptchaImageEngine.php";
--- unittest/Config/common.inc.php
+++ unittest/Config/common.inc.php
@@ -1,6 +1,6 @@
 <?php
 $lotusHome = substr(__FILE__, 0, strpos(__FILE__, "unittest"));
-require_once $lotusHome . "runtime/LtStore.php";
-require_once $lotusHome . "runtime/LtStoreMemory.php";
-require_once $lotusHome . "runtime/Config/Config.php";
-require_once $lotusHome . "runtime/Config/ConfigExpression.php";
+require_once $lotusHome . "runtime/Store.php";
+require_once $lotusHome . "runtime/StoreMemory.php";
+require_once $lotusHome . "runtime/Config.php";
+require_once $lotusHome . "runtime/ConfigExpression.php";
