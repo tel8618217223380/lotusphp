357f8d89f5d4737759046ba387ee451d043b9753
zhao5908 <zhao5908@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-04-10 03:07:15 +0000

    xcache测试通过环境为 Apache/2.2.15 (Win32) mod_fcgid/2.3.5 php nts 5.2.13

--- runtime/Cache/Adapter/CacheAdapterXcache.php
+++ runtime/Cache/Adapter/CacheAdapterXcache.php
@@ -6,31 +6,34 @@ class LtCacheAdapterXcache implements LtCacheAdapter
 		return true;
 	}
 
-	public function add($key, $value, $ttl=0, $tableName, $connectionResource)
+	public function add($key, $value, $ttl = 0, $tableName, $connectionResource)
 	{
 		return xcache_set($this->getRealKey($tableName, $key), $value, $ttl);
 	}
-	
+
 	public function del($key, $tableName, $connectionResource)
 	{
 		return xcache_unset($this->getRealKey($tableName, $key));
 	}
-	
+
 	public function get($key, $tableName, $connectionResource)
 	{
-		return xcache_get($this->getRealKey($tableName, $key));
+		$key = $this->getRealKey($tableName, $key);
+		if (xcache_isset($key))
+		{
+			return xcache_get($key);
+		}
+		return false;
 	}
 
 	public function update($key, $value, $ttl = 0, $tableName, $connectionResource)
 	{
-		if ($this->del($this->getRealKey($tableName, $key), $tableName, $connectionResource))
+		$key = $this->getRealKey($tableName, $key);
+		if (xcache_isset($key))
 		{
-			return $this->add($this->getRealKey($tableName, $key), $value, $ttl, $tableName, $connectionResource);
+			return xcache_set($key, $value, $ttl);
 		}
-		else
-		{
-			return false;
-		}
+		return false;
 	}
 
 	protected function getRealKey($tableName, $key)
