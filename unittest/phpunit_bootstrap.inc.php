0c811fae92d03d0d71038f41ddd326fc6868e17c
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-04-14 05:50:39 +0000

    small change

--- unittest/phpunit_bootstrap.inc.php
+++ unittest/phpunit_bootstrap.inc.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 修改以下配置为你实际环境的值
-如果你修改了这两个常量的值，请不要svn commit
+ * 如果你修改了这两个常量的值，请不要svn commit
  */
 define("PEAR_PATH", "D:/kiss/PHP/PEAR");
 define("LOTUS_UNITTEST_WEB_ROOT", "http://lotus/unittest/");
