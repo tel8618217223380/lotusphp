2768d10ccdb7b8a3a95fc4fcea1cf7c7bfa1d389
TalkativeDoggy <TalkativeDoggy@5139cd2e-8be0-11de-a4f2-b7219ab9ea91>
2010-11-23 13:34:35 +0000

    增加barebone的router配置

--- trunk/example/barebone/web_entrance/frontend/index.php
+++ trunk/example/barebone/web_entrance/frontend/index.php
@@ -2,5 +2,6 @@
 $lotusHome = "D:/lotus/trunk/runtime/";
 include($lotusHome . "Lotus.php");
 $lotus = new Lotus;
+$lotus->option["proj_dir"] = dirname(dirname(dirname(__FILE__)));
 $lotus->option["app_name"] = "frontend";
 $lotus->init();
